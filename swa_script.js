82ea56d2a1e26d4ff989e9422642c2c3e6fe66fe
em <63263677+subconstant@users.noreply.github.com>
2024-06-02 19:47:16 +0300

    update layout

--- swa_script.js
+++ swa_script.js
@@ -1,10 +1,43 @@
-function video(x){
-    document.getElementById('player').style.display = 'block';
-    document.getElementById('player').src = x;
+function video(x,y,z){
+    document.getElementById('play_window').style.opacity = '1';
+    document.getElementById('play_window').style.pointerEvents = 'all';
+
+    if( z == 'iframe'){
+        document.getElementById('i_player').style.display = 'block';
+        document.getElementById('i_player').src = x;
+    } else if( z == 'video'){
+        document.getElementById('v_player').style.display = 'block';
+        document.getElementById('v_player').src = x;
+    };
+
+    document.getElementById('fright').style.display = 'block';
+    document.getElementById('now_playing').innerHTML = y;
+}
+
+function back(){
+    document.getElementById('play_window').style.opacity = '0';
+    document.getElementById('play_window').style.pointerEvents = 'none';
+
+    document.getElementById('i_player').style.display = 'none';
+    document.getElementById('i_player').src = '';
+
+    document.getElementById('v_player').style.display = 'none';
+    document.getElementById('v_player').src = '';
+
+    document.getElementById('fright').style.display = 'none';
+}
+
+function win(x,y){
+    if (x == 1){
+        document.getElementById(y).style.opacity = '1';
+        document.getElementById(y).style.pointerEvents = 'all';
+    } else if(x == 0){
+        document.getElementById(y).style.opacity = '0';
+        document.getElementById(y).style.pointerEvents = 'none';
+    }
 }
 
 const moonlight = SunCalc.getMoonIllumination(new Date())['fraction'];
-//const moonlight = 0.7; // for testing
 document.getElementById('moonlight').innerHTML = Math.round(moonlight * 100);
 
 function close(){
@@ -19,4 +52,17 @@ function open(){
     document.body.style.color = "#00";
 }
 
-if(moonlight >= 0.90){ open() } else { close() };
+if(moonlight >= 0.90 || window.location.href.includes("?open") ){ open() } else { close() };
+
+function axes(){
+    document.querySelectorAll('.video_selector').forEach(function(x) {
+        x.style.left= Math.floor(Math.random() * 50) + 30 + 'vw';
+        x.style.top= Math.floor(Math.random() * 60) + 30 + 'vh';
+    });
+}
+
+$(document).ready(function() {
+    $(".draggable").draggable();
+});
+
+axes();
